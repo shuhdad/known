d4812ebce26dc5e7428791b19f8e0c45803559bc
wshuhd <wshuhd@gmail.com>
2022-08-04 01:04:45 +0800

    minHeadp sort

--- BinaryHeap/minHeap.js
+++ BinaryHeap/minHeap.js
@@ -47,7 +47,7 @@ class MinimumBinaryHeap {
                 this.arr[childIndex] = this.arr[parentIndex]
                 childIndex = parentIndex;
                 parentIndex = parseInt((childIndex - 1) / 2);
-            }else{
+            } else {
                 break;
             }
         }
@@ -62,21 +62,22 @@ class MinimumBinaryHeap {
      * @param {*} arr 
      * @param {*} pIndex 
      */
-    _downAdjust(parentIndex) {
-        if (parentIndex >= this.length) return;
+    _downAdjust(parentIndex, rear) {
+        rear = rear ?? this.length-1;
+        if (parentIndex > rear) return;
         let temp = this.arr[parentIndex];
         //左节点索引
         let childIndex = 2 * parentIndex + 1;
-        while (childIndex < this.length ) {
+        while (childIndex <= rear) {
             //如果存在右节点，并且右节点比左节点小，那么后面就用右节点替换
-            if (childIndex + 1 < this.length && this.arr[childIndex + 1] < this.arr[childIndex]) {
+            if (childIndex + 1 <= rear && this.arr[childIndex + 1] < this.arr[childIndex]) {
                 childIndex = childIndex + 1
             }
             if (temp > this.arr[childIndex]) {
                 this.arr[parentIndex] = this.arr[childIndex];
                 parentIndex = childIndex;
                 childIndex = 2 * parentIndex + 1
-            }else{
+            } else {
                 break;
             }
         }
@@ -86,9 +87,26 @@ class MinimumBinaryHeap {
         this.arr[parentIndex] = temp;
 
     }
+    /**
+     * 堆排序
+     * 最小堆，可以实现从大到小排
+     */
+    sort() {
+        for (let i = this.length - 1; i > 0; i--) {
+            //堆顶元素放末尾，末尾成为从大到小的有序区
+            let temp = this.arr[i]
+            this.arr[i] = this.arr[0]
+            this.arr[0] = temp;
+            this._downAdjust(0, i-1);
+        }
+    }
+
+
+
 }
 
 let bHeap = new MinimumBinaryHeap([7, 1, 3, 10, 5, 2, 8, 9, 6]);
 
-bHeap.insert(4)
-bHeap.delete(0)
+bHeap.sort();
+
+console.log(bHeap.arr)
