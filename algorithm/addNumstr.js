20b4809800039b9726a77b6635c5e26904ccd43a
Will Wang <wang_jian@trip.com>
2022-08-04 10:36:33 +0800

    2数相加

--- algorithm/addNumstr.js
+++ algorithm/addNumstr.js
@@ -2,30 +2,30 @@ function add(a, b) {
     let rstNumber = ""
     let m = a.length - 1;
     let n = b.length - 1;
-    if (m <= 0 && n <= 0) {
+    if (m < 0 && n < 0) {
         return 0;
     }
     let extraAddNum = 0;
     while (m >= 0 || n >= 0) {
         let mNum = m < 0 ? 0 : parseInt(a[m]);
-        let nUmber = n < 0 ? 0 : parseInt(a[n]);
-        let cur = mNum + nUmber + extraAddNum;
-        if (cur > 10) {
+        let nNum = n < 0 ? 0 : parseInt(b[n]);
+        let cur = mNum + nNum + extraAddNum;
+        if (cur >= 10) {
             extraAddNum = 1
             cur = cur % 10;
+        } else {
+            extraAddNum = 0
         }
-
         rstNumber = cur + rstNumber
-
-
         m--;
         n--
     }
 
-    return extraAddNum + rstNumber;
+    return (extraAddNum ? extraAddNum : "") + rstNumber;
 
 }
-
-let rst = add("123","234")
-
-console.log(rst);
+let an = "998";
+let bn = "123";
+let rst = add(an, bn)
+let rst2 = parseInt(an) + parseInt(bn);
+console.log(rst, rst2);
